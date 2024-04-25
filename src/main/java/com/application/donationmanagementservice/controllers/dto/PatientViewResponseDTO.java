82ea8c04344d35addf5b2888f5ce54ebc64ab515
imexh <chandikaranawella@gmail.com>
2024-04-25 20:00:20 +0530

    removed patients

--- src/main/java/com/application/donationmanagementservice/controllers/dto/PatientViewResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package com.application.donationmanagementservice.controllers.dto;
-
-import com.application.donationmanagementservice.common.value.Patient;
-
-public record PatientViewResponseDTO(Patient patient) {
-}
