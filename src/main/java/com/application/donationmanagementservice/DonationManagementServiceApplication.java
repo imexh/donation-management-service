08874e385eb9fbabe98a2b0cff768fbc082f47ff
imexh <chandikaranawella@gmail.com>
2024-04-25 19:07:28 +0530

    first commit

---
+++ src/main/java/com/application/donationmanagementservice/DonationManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.donationmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonationManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/donationmanagementservice/DonationManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.donationmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
